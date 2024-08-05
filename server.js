ea197b986bbde074a28562746a6c6feec743e3b4
Vypboy <vytran210402@gmail.com>
2024-08-05 11:44:47 +0700

    t

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World123!'))
+app.get('/', (req, res) => res.send('Hello World12312321!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
